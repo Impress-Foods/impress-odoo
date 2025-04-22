b098c7b7bfbc6f8b0bbb217e7ee3ac593506a105
Cédric Paradis <cparadis@impressfoods.com>
2025-04-22 10:40:33 -0400

    Logic for additional states done + unit tests

---
+++ delivery_status_multi_step/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Delivery Status Multi Step",
+    "version": "17.0.1.0.0",
+    "summary": """ Adds usefull delivery statuses for multi-step delivery flows """,
+    "author": "Cédric Paradis",
+    "website": "",
+    "category": "Hidden",
+    "depends": ["base", "sale_stock"],
+    "data": [],
+    "installable": True,
+    "auto_install": ["sale_stock"],
+    "license": "GPL-2",
+}
