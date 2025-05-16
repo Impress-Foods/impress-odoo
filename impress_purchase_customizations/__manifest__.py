ab20583b23ae19210dbb06b62da5b1d154106fb0
Cédric Paradis <cparadis@impressfoods.com>
2025-05-16 12:39:27 -0400

    Added impress_pruchase_customizations with new PO reports

---
+++ impress_purchase_customizations/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Impress-purchase-customizations",
+    "version": "17.0.1.0.0",
+    "summary": """ Impress-purchase-customizations Summary """,
+    "author": "Cédric Paradis",
+    "website": "https://github.com/cparadis-impressfoods/impress-odoo",
+    "category": "Hidden",
+    "depends": ["base", "purchase"],
+    "data": [
+        "report/purchase_order_report_inherit.xml",
+        "report/purchase_order_report.xml",
+    ],
+    "application": False,
+    "installable": True,
+    "auto_install": False,
+    "license": "GPL-2",
+}
