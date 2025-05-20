be2677e524b2e5ba9d95c7c348aa7ba26c0e7855
Cédric Paradis <cparadis@impressfoods.com>
2025-05-20 14:37:43 -0400

    Understood how dynamic templates are working

--- theme_aplus/static/src/snippets/s_dynamic_snippet_aplus_carousel/000.js
+++ theme_aplus/static/src/snippets/s_dynamic_snippet_aplus_carousel/000.js
@@ -4,7 +4,15 @@ import publicWidget from "@web/legacy/js/public/public_widget";
 import DynamicSnippetProducts from "@website_sale/snippets/s_dynamic_snippet_products/000";
 
 const aplusDynamicSnippetProducts = DynamicSnippetProducts.extend({
-    selector: ".s_dynamic_snippet_aplus_carousel",
+    selector: ".aplus_product_carousel",
+
+    /**
+     * @override
+     */
+    init: function () {
+        this._super.apply(this, arguments);
+        this.template_key = "theme_aplus.aplus_product";
+    },
 });
 
 publicWidget.registry.dynamic_snippet_aplus_carousel = aplusDynamicSnippetProducts;
--- theme_aplus/static/src/snippets/s_dynamic_snippet_aplus_carousel/options.js
+++ theme_aplus/static/src/snippets/s_dynamic_snippet_aplus_carousel/options.js
@@ -2,43 +2,7 @@
 import options from "@web_editor/js/editor/snippets.options";
 import dynamicSnippetProductsOptions from "@website_sale/snippets/s_dynamic_snippet_products/options";
 
-const aplusDynamicSnippetProductOptions = dynamicSnippetProductsOptions.extend({
-    /**
-     * @override
-     */
-    async willStart() {
-        const _super = this._super.bind(this);
-        const result = _super(...arguments);
-        console.log(this.dynamicFilterTemplates);
-        return result;
-    },
+const aplusDynamicSnippetProductOptions = dynamicSnippetProductsOptions.extend({});
 
-    /**
-     * @private
-     * @override
-     */
-    async _fetchDynamicFilterTemplates() {
-        const filter =
-            this.dynamicFilters[this.$target.get(0).dataset["filterId"]] ||
-            this.dynamicFilters[this._defaultFilterId];
-        this.dynamicFilterTemplates = {};
-        if (!filter) {
-            return [];
-        }
-        const dynamicFilterTemplates = await this.rpc(
-            "/website/snippet/filter_templates",
-            {
-                filter_name: filter.model_name.replaceAll(".", "_"),
-            }
-        );
-        console.log(dynamicFilterTemplates);
-        for (let index in dynamicFilterTemplates) {
-            this.dynamicFilterTemplates[dynamicFilterTemplates[index].key] =
-                dynamicFilterTemplates[index];
-        }
-        this._defaultTemplateKey = dynamicFilterTemplates[0].key;
-    },
-});
-
-options.registry.dynamic_snippet_aplus_carousel = aplusDynamicSnippetProductOptions;
+options.registry.aplus_product_carousel = aplusDynamicSnippetProductOptions;
 export default aplusDynamicSnippetProductOptions;
