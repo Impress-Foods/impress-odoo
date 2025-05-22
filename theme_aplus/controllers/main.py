ae1b0feedbe2366324204dff9247824da22b7bd8
Cédric Paradis <cparadis@impressfoods.com>
2025-05-22 08:19:46 -0400

    Data fetching for carousel functional

---
+++ theme_aplus/controllers/main.py
@@ -0,0 +1,39 @@
+from ast import List
+import logging
+
+from odoo import http
+from odoo.http import request
+
+_logger = logging.getLogger(__name__)
+
+
+class ThemeAPlus(http.Controller):
+    @http.route(
+        "/theme_aplus/aplus_product_carousel", type="http", auth="public", website=True
+    )
+    def aplus_product_carousel(self):
+        return http.request.render("theme_aplus.aplus_product_carousel")
+
+    @http.route("/theme_aplus/get_products", type="json", auth="public", website=True)
+    def get_products(self, filter_id):
+        dynamic_filter = (
+            request.env["website.snippet.filter"]
+            .sudo()
+            .search([("id", "=", filter_id)] + request.website.website_domain())
+        )
+        raw_values_list = dynamic_filter._prepare_values()
+
+        values_list = {
+            i: self._process_values(values)
+            for i, values in enumerate(raw_values_list)  # type: ignore
+        }
+        return values_list
+
+    def _process_values(self, data):
+        values = {
+            "display_name": data["display_name"],
+            "image_512": data["image_512"],
+            "product_id": data["product_id"],
+        }
+        # _logger.warning(values)
+        return values
--- theme_aplus/static/src/snippets/s_dynamic_snippet_aplus_carousel/000.js
+++ theme_aplus/static/src/snippets/s_dynamic_snippet_aplus_carousel/000.js
@@ -5,6 +5,9 @@ import DynamicSnippetProducts from "@website_sale/snippets/s_dynamic_snippet_pro
 
 const aplusDynamicSnippetProducts = DynamicSnippetProducts.extend({
     selector: ".aplus_product_carousel",
+    events: {
+        "click .scroller-item": "_onScrollerItemClick",
+    },
 
     /**
      * @override
@@ -12,6 +15,48 @@ const aplusDynamicSnippetProducts = DynamicSnippetProducts.extend({
     init: function () {
         this._super.apply(this, arguments);
         this.template_key = "theme_aplus.aplus_product";
+        this.product_data = [];
+    },
+
+    /**
+     * @override
+     */
+    _render() {
+        const self = this;
+        if (self.carousel) {
+            self.carousel.dispose();
+        }
+        return this._super.apply(this, arguments);
+    },
+
+    _onScrollerItemClick: function (event) {},
+
+    async _fetchData() {
+        this._super.apply(this, arguments);
+
+        if (this._isConfigComplete()) {
+            const nodeData = this.el.dataset;
+            //console.log(nodeData);
+            const response = await this.rpc(
+                "/theme_aplus/get_products",
+                Object.assign({
+                    filter_id: parseInt(nodeData.filterId),
+                }),
+                this._getRpcParameters()
+            );
+            this.product_data = response;
+        } else {
+            this.product_data = [];
+        }
+    },
+
+    /**
+     * @override
+     */
+    _getQWebRenderOptions() {
+        const result = this._super(...arguments);
+        result["product_data"] = this.product_data;
+        return result;
     },
 });
 
