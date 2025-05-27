f3aa66bf3a9ac5782dfdb30fa4498e409e0bff1d
Cédric Paradis <cparadis@impressfoods.com>
2025-05-27 13:37:10 -0400

    added guard against multiple lots for lot w/ qty label

--- label_printing_wizard/report/lot_labels.py
+++ label_printing_wizard/report/lot_labels.py
@@ -1,6 +1,7 @@
 import logging
 
 from odoo import models, _
+from odoo.exceptions import UserError
 
 _logger = logging.getLogger(__name__)
 
@@ -11,6 +12,7 @@ class ReportLotLabel(models.AbstractModel):
     def _get_report_values(self, docids, data):
         res = super()._get_report_values(docids, data)
         if 'label_product_qty' in self.env.context:
+            if len(res['docs']) != 1:
+                raise UserError(_("Only one lot can be selected"))
             res['docs'][0]['product_qty'] = self.env.context.get('label_product_qty')
         return res
-7
