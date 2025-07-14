80ee579c5912d482f4673454b3defb73a2e70370
Cédric Paradis <cparadis@impressfoods.com>
2025-07-14 10:18:51 -0400

    Fixed check digit calculation when handling multiple of 10 sum (#62)
    * Fixed check digit calculation when handling multiple of 10 sum
    
    * Added tests to gs1_sequences

--- gs1_sequences/models/ir_sequence.py
+++ gs1_sequences/models/ir_sequence.py
@@ -1,6 +1,7 @@
 import logging
 
-from odoo import api, fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 _logger = logging.getLogger(__name__)
 
@@ -12,18 +13,26 @@ class IrSequence(models.Model):
 
     @api.model
     def _gs1_check_digit(self, code):
+        if not code.isnumeric() or len(code) == 0:
+            raise ValidationError(_("Code must be numeric"))
+
         code = [int(x) for x in code]
 
         odds = code[0::2]
         evens = code[1::2]
 
-        check_digit = 10 - (3 * sum(odds) + sum(evens)) % 10
-
+        sum_of_digits = 3 * sum(odds) + sum(evens)
+        nearest_ten = (
+            ((sum_of_digits // 10) + 1) * 10
+            if sum_of_digits % 10 != 0
+            else sum_of_digits
+        )
+        check_digit = nearest_ten - sum_of_digits
         return str(check_digit)
 
     def get_next_char(self, number_text):
         res = super().get_next_char(number_text)
-        if self.check_digit and res.isnumeric():
+        if self.check_digit:
             check_digit = self._gs1_check_digit(res)
             res += check_digit
         return res
