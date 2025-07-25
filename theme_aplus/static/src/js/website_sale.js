747e52f1b56c1e41d01ea6a0c06108f1a1266b8a
Cedric Paradis <cparadis@impressfoods.com>
2025-07-25 14:57:34 +0000

    Fixed small bug with highlight color in product page

--- theme_aplus/static/src/js/website_sale.js
+++ theme_aplus/static/src/js/website_sale.js
@@ -3,33 +3,27 @@ import {WebsiteSale} from "@website_sale/js/website_sale";
 import {patch} from "@web/core/utils/patch";
 
 patch(WebsiteSale.prototype, {
-    // start() {
-    //     const def = super.start();
-    //     document
-    //         .querySelector(":root")
-    //         .style.setProperty(
-    //             "--product-page-highlight-color",
-    //             this.el.dataset.highlightColor
-    //         );
-    //     console.log(this);
-    //     return def;
-    // },
-
     willStart() {
         const res = super.willStart(...arguments);
-        document
-            .querySelector(":root")
-            .style.setProperty(
-                "--product-page-highlight-color",
-                this.el.dataset.highlightColor
-            );
-        document
-            .querySelector(":root")
-            .style.setProperty(
-                "--product-page-highlight-sec-color",
-                this.el.dataset.highlightSecColor
-            );
-        console.log(this);
+        if (this.el.dataset) {
+            if (this.el.dataset.highlightColor) {
+                document
+                    .querySelector(":root")
+                    .style.setProperty(
+                        "--product-page-highlight-color",
+                        this.el.dataset.highlightColor
+                    );
+            }
+            if (this.el.dataset.highlightSecColor) {
+                document
+                    .querySelector(":root")
+                    .style.setProperty(
+                        "--product-page-highlight-sec-color",
+                        this.el.dataset.highlightSecColor
+                    );
+            }
+        }
+
         return res;
     },
 });
