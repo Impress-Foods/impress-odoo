1044ccf445fc41e424efa2353babb1bd77cb43da
Cedric Paradis <cparadis@impressfoods.com>
2025-07-28 20:04:12 +0000

    Working on responsive design for the theme

--- theme_aplus/static/src/snippets/s_dynamic_snippet_product_templates/000.js
+++ theme_aplus/static/src/snippets/s_dynamic_snippet_product_templates/000.js
@@ -5,6 +5,11 @@ import publicWidget from "@web/legacy/js/public/public_widget";
 
 const DynamicSnippetProductTemplates = DynamicSnippetProducts.extend({
     selector: ".s_dynamic_snippet_product_templates",
+
+    init: function () {
+        this._super.apply(this, arguments);
+        this.template_key = "theme_aplus.s_dynamic_snippet.carousel";
+    },
 });
 
 publicWidget.registry.dynamic_snippet_product_templates =
