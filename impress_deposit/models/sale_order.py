ef0e6044698028ab84dd64e8d821f53118da1877
Cedric Paradis <cparadis@impressfoods.com>
2025-08-08 12:23:28 +0000

    Added tests to impress_deposit and reworked logic

--- impress_deposit/models/sale_order.py
+++ impress_deposit/models/sale_order.py
@@ -2,6 +2,8 @@ import logging
 
 from odoo import _, api, fields, models
 
+from odoo.addons.sale.models.sale_order_line import SaleOrderLine
+
 _logger = logging.getLogger(__name__)
 
 
@@ -9,78 +11,85 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     deposit_value = fields.Monetary(
-        string="Deposit", compute="_compute_deposit_value", store=True
+        string="Deposit",
+        compute="_compute_deposit_value",
+        store=True,
+        depends=["order_line.qty_delivered", "order_line.product_uom_qty", "state"],
     )
+    test = fields.Boolean(compute="_compute_test", store=True)
 
-    @api.depends("order_line.qty_delivered", "order_line.product_uom_qty")
-    def _compute_deposit_value(self):
+    @api.depends("state", "order_line")
+    def _compute_test(self) -> None:
+        _logger.warning("Test Compute Method")
+        self.test = True
+
+    @api.depends("order_line.qty_delivered", "order_line.product_uom_qty", "state")
+    def _compute_deposit_value(self) -> None:
+        _logger.warning("Computing deposit value")
         for record in self:
             if record._deposit_needed():
-                record._handle_deposit()
+                deposit_line = record._get_deposit_line()
+                deposit_line.write(
+                    {
+                        "qty_delivered": self._compute_container_count(),
+                        "product_uom_qty": self._compute_container_count(),
+                    }
+                )
+                record.deposit_value = deposit_line.price_total
             else:
                 record.deposit_value = 0.0
 
-    def _deposit_needed(self):
-        # TODO: Figure out better way to differentiate between
-        # eCom orders (both from Odoo and WooCommcerce)
-        # and B2B orders.
-
+    def _deposit_needed(self) -> bool:
         self.ensure_one()
         products_need_deposit = any(
             self.order_line.mapped(lambda x: x.product_id.requires_deposit)
         )
         partner_need_deposit = self.partner_id.requires_deposit
         order_stage = self.state not in ["cancel", "draft", "sent"]
+        # _logger.warning(
+        #     f"""product: {products_need_deposit} \n
+        #         partner: {partner_need_deposit} \n
+        #         stage: {order_stage}"""
+        # )
+        return all([products_need_deposit, partner_need_deposit, order_stage])
 
-        b2b = all([products_need_deposit, partner_need_deposit, order_stage])
-        ecom = all([self.website_id])
-        return any([b2b, ecom])
-
-    def _handle_deposit(self):
+    def _get_deposit_line(self) -> SaleOrderLine:
         self.ensure_one()
+        lines = self.order_line.filtered(lambda x: x.is_deposit_line)
+        deposit_line = self.env["sale.order.line"]
+        match len(lines):
+            case 0:
+                deposit_product_id = int(
+                    self.env["ir.config_parameter"]
+                    .sudo()
+                    .get_param("impress_deposit.deposit_product")
+                )
+                deposit_product = self.env["product.product"].browse(deposit_product_id)
+                deposit_line = self.env["sale.order.line"].create(
+                    [
+                        {
+                            "order_id": self.id,
+                            "name": _("Deposit"),
+                            "product_id": deposit_product.id,
+                            "product_uom": deposit_product.uom_id.id,
+                            "qty_delivered": 0,
+                            "product_uom_qty": 0,
+                            "is_deposit_line": True,
+                        }
+                    ]
+                )
+            case 1:
+                deposit_line = lines[0]
 
-        deposit_product_id = int(
-            self.env["ir.config_parameter"]
-            .sudo()
-            .get_param("impress_deposit.deposit_product")
-        )
-        deposit_product = self.env["product.product"].browse(deposit_product_id)
-        deposit_line = self.env["sale.order.line"].search(
-            [("order_id", "=", self.id), ("product_id", "=", deposit_product_id)]
-        )
-        if len(deposit_line) == 0:
-            deposit_line = self.env["sale.order.line"].create(
-                [
-                    {
-                        "order_id": self.id,
-                        "name": _("Deposit"),
-                        "product_id": deposit_product.id,
-                        "product_uom": deposit_product.uom_id.id,
-                        "qty_delivered": self._compute_container_count(),
-                        "product_uom_qty": self._compute_container_count(),
-                        "is_deposit_line": True,
-                    }
-                ]
-            )
+            case _:
+                deposit_line = lines[0]
+                (lines - deposit_line).unlink()
 
-        else:
-            deposit_line = deposit_line[0]
-            deposit_line.write(
-                {
-                    "qty_delivered": 0
-                    if self.website_id
-                    else self._compute_container_count(),
-                    "product_uom_qty": self._compute_container_count(),
-                }
-            )
-        self.deposit_value = deposit_line.price_total
+        return deposit_line
 
-    def _compute_container_count(self):
+    def _compute_container_count(self) -> int:
         total = 0
         for line in self.order_line:
-            if line.product_id.requires_deposit:
-                if self.website_id:
-                    total += line.product_uom_qty * line.product_id.qty_multiple
-                else:
-                    total += line.qty_delivered * line.product_id.qty_multiple
+            total += line.get_deposit_container_qty()
+
         return total
--- impress_deposit/models/sale_order_line.py
+++ impress_deposit/models/sale_order_line.py
@@ -9,3 +9,10 @@ class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     is_deposit_line = fields.Boolean(default=False)
+
+    def get_deposit_container_qty(self) -> int:
+        self.ensure_one()
+        if self.product_id.requires_deposit:
+            return int(self.qty_delivered * self.product_id.qty_multiple)
+        else:
+            return 0
