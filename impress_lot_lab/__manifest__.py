9356e19fa8288932c73278fd86a1c99d0bf967bd
Cédric Paradis <cparadis@impressfoods.com>
2025-09-19 14:05:36 -0400

    19.0-lot-modules (#131)
    * [MIG][19.0] impress_lot_lab
    Fixes #130
    
    * [MIG][19.0] impress_expiration_lot
    Fixes #129

---
+++ impress_lot_lab/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Impress_lot_lab",
+    "version": "19.0.0.1.2",
+    "summary": """ Impress_lot_lab Summary """,
+    "author": "Cédric Paradis",
+    "website": "https://github.com/impress-foods/impress-odoo",
+    "category": "Hidden",
+    "depends": ["stock", "quality_control"],
+    "data": ["data/stock_lot.xml", "views/impress_lot_lab_lot_views.xml"],
+    "installable": True,
+    "license": "GPL-2",
+}
