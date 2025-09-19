7ee68cce1e26a2b7279021c99be76765f4e28b16
Cédric Paradis <cparadis@impressfoods.com>
2025-09-19 09:40:27 -0400

    19.0-sequences (#117)
    * [MIG][19.0] gs1_sequences
    Fixes #116
    
    * [MIG][19.0] julian_sequence
    Fixes #115

---
+++ gs1_sequences/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "GS1 Sequences",
+    "version": "19.0.1.0.0",
+    "summary": """ Adds GS1 check digit option on sequences """,
+    "author": "Cédric Paradis",
+    "website": "https://github.com/impress-foods/impress-odoo",
+    "category": "Hidden",
+    "depends": [
+        "base",
+    ],
+    "data": ["views/ir_sequence_views.xml"],
+    "installable": True,
+    "auto_install": False,
+    "license": "GPL-2",
+}
---
+++ julian_sequence/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Julian Sequence for MRP",
+    "summary": """ Adds a sequence type to follow a YYDDD format""",
+    "author": "Cédric Paradis",
+    "website": "https://github.com/impress-foods/impress-odoo",
+    "license": "GPL-2",
+    "category": "Hidden",
+    "version": "19.0.0.1.0",
+    "depends": ["mrp", "stock"],
+    "installable": True,
+}
