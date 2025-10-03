47cd07e144c7bdc6f7a7f746d47ac127aa1d2cc5
Cedric Paradis <cparadis@impressfoods.com>
2025-10-03 16:47:46 +0000

    [MIG][19.0] maintenance_sequence Fixes #154

---
+++ maintenance_sequence/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Maintenance_sequence",
+    "version": "19.0.1.0.0",
+    "summary": """ Maintenance_sequence Summary """,
+    "author": "Cédric Paradis",
+    "website": "https://github.com/impress-foods/impress-odoo",
+    "category": "Hidden",
+    "depends": ["base", "maintenance"],
+    "data": ["data/ir_sequence.xml", "views/maintenance_request_views.xml"],
+    "installable": True,
+    "license": "GPL-2",
+}
