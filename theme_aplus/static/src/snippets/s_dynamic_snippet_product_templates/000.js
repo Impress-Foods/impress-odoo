f0fde7083a6ddfc152f5b13569bafc29d02f8d90
Cedric Paradis <cparadis@impressfoods.com>
2025-10-10 18:02:00 +0000

    Responsiveness with minimalist carousel + dynamic product template tag fix

--- theme_aplus/static/src/snippets/s_dynamic_snippet_product_templates/000.js
+++ theme_aplus/static/src/snippets/s_dynamic_snippet_product_templates/000.js
@@ -10,6 +10,19 @@ const DynamicSnippetProductTemplates = DynamicSnippetProducts.extend({
         this._super.apply(this, arguments);
         this.template_key = "theme_aplus.s_dynamic_snippet.carousel";
     },
+    _getTagSearchDomain: function () {
+        const searchDomain = [];
+        let productTagIds = this.$el.get(0).dataset.productTagIds;
+        productTagIds = productTagIds ? JSON.parse(productTagIds) : [];
+        if (productTagIds.length) {
+            searchDomain.push([
+                "product_tag_ids",
+                "in",
+                productTagIds.map((productTag) => productTag.id),
+            ]);
+        }
+        return searchDomain;
+    },
 });
 
 publicWidget.registry.dynamic_snippet_product_templates =
