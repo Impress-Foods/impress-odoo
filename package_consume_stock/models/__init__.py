4d516ffac4e956dbf90d8b3cd4d2add277a2cb57
Cédric Paradis <cparadis@impressfoods.com>
2025-11-18 15:25:15 -0500

    [17.0][IMP] Changed package_consume_stock to allow multiple packaging material (#171)

--- package_consume_stock/models/__init__.py
+++ package_consume_stock/models/__init__.py
@@ -1,3 +1,4 @@
+from . import stock_package_material
 from . import stock_package_type
 from . import stock_picking
 from . import stock_quant_package
--- package_consume_stock/models/stock_package_type.py
+++ package_consume_stock/models/stock_package_type.py
@@ -1,6 +1,6 @@
 import logging
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 _logger = logging.getLogger(__name__)
 
@@ -8,5 +8,13 @@ _logger = logging.getLogger(__name__)
 class PackageType(models.Model):
     _inherit = "stock.package.type"
 
-    packaging_material_id = fields.Many2one("product.product")
-    source_location_id = fields.Many2one("stock.location")
+    packaging_material_ids = fields.Many2many("stock.package.material")
+    has_packaging_material = fields.Boolean(compute="_compute_has_packaging_material")
+
+    @api.depends("packaging_material_ids")
+    def _compute_has_packaging_material(self):
+        for record in self:
+            if len(record.packaging_material_ids) > 0:
+                record.has_packaging_material = True
+            else:
+                record.has_packaging_material = False
