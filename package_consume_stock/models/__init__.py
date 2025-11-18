9b39f5a90e8dfc281812597b2b927e2dbf2f2d3b
Cédric Paradis <cparadis@impressfoods.com>
2025-11-18 15:25:15 -0500

    [19.0][IMP] Changed package_consume_stock to allow multiple packaging material (#171)

--- package_consume_stock/models/__init__.py
+++ package_consume_stock/models/__init__.py
@@ -1,3 +1,4 @@
+from . import stock_package_material
 from . import stock_package_type
 from . import stock_package
 from . import stock_move_line
--- package_consume_stock/models/stock_move_line.py
+++ package_consume_stock/models/stock_move_line.py
@@ -14,45 +14,21 @@ class StockMoveLine(models.Model):
         return res
 
     def _handle_new_package(self):
-        for move_line in self:
-            picking = move_line.picking_id
-            for package in move_line.result_package_id:
-                if not package.material_added and package.package_type_id:
-                    package_type = package.package_type_id
+        for picking in self:
+            for package in picking.package_ids:
+                if (
+                    not package.material_added
+                    and package.package_type_id.has_packaging_material
+                ):
+                    for material in package.package_type_id.packaging_material_ids:
+                        source_location = material.location_id or picking.location_id
 
-                    source_location = (
-                        package_type.source_location_id or picking.location_id
-                    )
-                    product = package_type.packaging_material_id or None
+                        lot_id = material._get_packaging_lot(picking)
 
-                    if product:
-                        # Ensure there is a corresponding move
-                        # for the packaging material
-                        packaging_move = picking.move_ids.filtered_domain(
-                            [("product_id", "=", product.id)]
-                        )[:1]
-
-                        lot_id = False
+                        packaging_move = material._get_packaging_material_move(picking)
 
                         if not packaging_move:
-                            packaging_move = self.env["stock.move"].create(
-                                {
-                                    "picking_id": picking.id,
-                                    "product_id": product.id,
-                                    "product_uom_qty": 0,
-                                    "quantity": 0,
-                                    "location_id": source_location.id,
-                                    "location_dest_id": picking.location_dest_id.id,
-                                    "company_id": picking.company_id.id,
-                                    "move_line_ids": False,
-                                }
-                            )
-
-                        else:
-                            packaging_move = packaging_move[0]
-                            if product.tracking in ["serial", "lot"]:
-                                if packaging_move.move_line_ids:
-                                    lot_id = packaging_move.move_line_ids[0].lot_id.id
+                            continue
 
                         self.env["stock.move.line"].create(
                             {
@@ -62,13 +38,12 @@ class StockMoveLine(models.Model):
                                 "location_id": source_location.id,
                                 "location_dest_id": picking.location_dest_id.id,
                                 "result_package_id": package.id,
-                                "product_id": product.id,
-                                "quantity": 1,
-                                "qty_done": 1,
+                                "product_id": material.product_id.id,
+                                "quantity": material.quantity,
+                                "qty_done": material.quantity,
                                 "lot_id": lot_id,
                                 "move_id": packaging_move.id,
                             }
                         )
-
-                        packaging_move.product_uom_qty += 1
+                        packaging_move.product_uom_qty += material.quantity
                         package.material_added = True
