233bc5271596f221ec035437b525edb90a8b89f3
Cédric Paradis <cparadis@impressfoods.com>
2025-11-25 08:32:12 -0500

    [17.0][IMP] Added remaining shelf life on lot form, report_stock_picking and report_deliveryslip (#175)

--- impress_stock_customizations/__manifest__.py
+++ impress_stock_customizations/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Impress Stock Customizations",
-    "version": "17.0.0.1.1",
-    "depends": ["base", "stock"],
+    "version": "17.0.0.2.1",
+    "depends": ["base", "stock", "product_expiry"],
     "author": "Cédric Paradis",
     "website": "https://github.com/Impress-Foods/impress-odoo",
     "category": "Inventory",
@@ -14,5 +14,7 @@
         "reports/impress_stock_customizations_stock_delivery_document_views.xml",
         "reports/impress_stock_customizations_labels.xml",
         "reports/online_sale_labels.xml",
+        "views/stock_lot_views.xml",
+        "reports/stock_picking_document_views.xml",
     ],
 }
