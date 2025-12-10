bfd0a21d49aeca4389cd994405ca832084e4ebe2
Cedric Paradis <cparadis@impressfoods.com>
2025-12-10 19:03:03 +0000

    [17.0][FIX] Fixed "stored" field to "store" in sale customizations

--- impress_sales_customizations/models/sale_order.py
+++ impress_sales_customizations/models/sale_order.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    delivery_zip = fields.Char(related="partner_shipping_id.zip", stored=True)
+    delivery_zip = fields.Char(related="partner_shipping_id.zip", store=True)
