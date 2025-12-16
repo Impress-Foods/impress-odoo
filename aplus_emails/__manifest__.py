6d0d78d65c5bcb42fb00891d4ff353060d00c6bf
Cedric Paradis <cparadis@impressfoods.com>
2025-12-16 17:08:31 +0000

    Bumped version

--- aplus_emails/__manifest__.py
+++ aplus_emails/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Emails Templates - A+",
-    "version": "17.0.1.0.1",
+    "version": "17.0.1.0.4",
     "summary": """ Email templates for A+ """,
     "author": "Cédric Paradis",
     "website": "https://github.com/Impress-Foods/impress-odoo",
