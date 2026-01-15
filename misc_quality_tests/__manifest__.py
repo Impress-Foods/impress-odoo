25550082d1d5a002ecf992363361a13378275d46
Cedric Paradis <cparadis@impressfoods.com>
2026-01-15 13:42:52 +0000

    [17.0][IMP] Init module

---
+++ misc_quality_tests/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Miscellaneous Quality Tests",
+    "version": "17.0.0.0.0",
+    "summary": """ Misc_quality_tests Summary """,
+    "author": "Cédric Paradis",
+    "website": "https://github.com/Impress-Foods/impress-odoo",
+    "category": "Hidden",
+    "depends": ["base", "stock"],
+    "data": [],
+    "application": False,
+    "installable": True,
+    "auto_install": False,
+    "license": "GPL-2",
+}
