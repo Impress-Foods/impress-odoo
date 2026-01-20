1c631427b9402c33bd84a9acabe4e63853c294ce
Cédric Paradis <cparadis@impressfoods.com>
2026-01-20 09:12:08 -0500

    [17.0] Implemented migration script (#193)

--- impress_quality_logs/__manifest__.py
+++ impress_quality_logs/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Impress Quality Logs",
-    "version": "17.0.0.1.3",
+    "version": "17.0.0.1.4",
     "summary": """ Implements many quality logs used by ^
     Impress Foods for quality control """,
     "author": "Cédric Paradis",
