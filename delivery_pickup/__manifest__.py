d54459bb6e6579ac428f9c850c81e33f67afcd79
Cedric Paradis <cparadis@impressfoods.com>
2026-01-27 17:34:36 +0000

    [19.0][MIG] Added delivery_pickup form 17.0

---
+++ delivery_pickup/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Delivery - Pickup",
+    "version": "19.0.1.0.0",
+    "summary": """ Pickup Delivery Connector """,
+    "author": "Cédric Paradis",
+    "website": "https://github.com/Impress-Foods/impress-odoo",
+    "category": "Inventory/Delivery",
+    "depends": ["stock_delivery", "mail", "delivery_common"],
+    "data": ["views/delivery_carrier_views.xml", "reports/report_pickup_label.xml"],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+    "license": "GPL-2",
+}
