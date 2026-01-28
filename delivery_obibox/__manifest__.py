c0dfa42c10362eabb4589a92641cbb2be3244a10
Cedric Paradis <cparadis@impressfoods.com>
2026-01-28 18:16:34 +0000

    [19.0][MIG] Copied files from 17.0

---
+++ delivery_obibox/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Delivery - Obibox",
+    "version": "19.0.1.0.0",
+    "summary": """ Obibox Delivery Connector """,
+    "author": "Cédric Paradis",
+    "website": "https://github.com/impress-foods/impress-odoo",
+    "category": "Inventory/Delivery",
+    "depends": ["stock_delivery", "mail", "delivery_common"],
+    "external_dependencies": {
+        "python": [
+            "pydantic>=2.0.0",
+        ]
+    },
+    "data": ["views/delivery_carrier_views.xml"],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+    "license": "GPL-2",
+}
