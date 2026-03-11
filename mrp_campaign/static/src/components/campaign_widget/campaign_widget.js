be2ad577de607fd6060305fbed818b957e93f54e
Cédric Paradis <cparadis@impressfoods.com>
2026-03-11 12:43:38 +0000

    [17.0][FIX] Removed unused import

--- mrp_campaign/static/src/components/campaign_widget/campaign_widget.js
+++ mrp_campaign/static/src/components/campaign_widget/campaign_widget.js
@@ -1,12 +1,10 @@
 /** @odoo-module **/
 import {registry} from "@web/core/registry";
 import {CampaignOrchestrator} from "./campaign_orchestrator/campaign_orchestrator";
-import {standardFieldProps} from "@web/views/fields/standard_field_props";
 
 export const CampaignWidgetField = {
     component: CampaignOrchestrator,
     supportTypes: ["char"],
 };
 
-// Registering it so you can use widget="campaign_orchestrator" in XML
 registry.category("fields").add("campaign_orchestrator", CampaignWidgetField);
