881260b713af60466fd3562e7c50381979e8cb8b
Cédric Paradis <cparadis@impressfoods.com>
2026-03-16 19:02:26 +0000

    [17.0][FIX] Moved sale related logic to mrp_campaign_sale

--- mrp_campaign/tests/test_stock_move.py
+++ mrp_campaign/tests/test_stock_move.py
@@ -77,37 +77,3 @@ class TestStockMove(CampaignCase):
 
         self.assertEqual(move.campaign_qty_to_supply, QTY)
         self.assertTrue(move.campaign_can_be_added)
-
-    def test_sale_customer_ref(self) -> None:
-        """Test that sale_customer_ref correctly extracts
-        client_order_ref from sale order."""
-        move = self.env["stock.move"].create(
-            {
-                "name": "move",
-                "product_id": self.end_prod_a_red.id,
-                "product_uom_qty": 10.0,
-                "location_id": self.stock_location.id,
-                "location_dest_id": self.stock_location.id,
-            }
-        )
-
-        self.assertEqual(move.sale_customer_ref, "")
-
-        if "sale_line_id" in self.env["stock.move"]:
-            partner = self.env["res.partner"].create({"name": "Test Customer"})
-            sale_order = self.env["sale.order"].create(
-                {
-                    "partner_id": partner.id,
-                    "client_order_ref": "PO-12345",
-                }
-            )
-            sale_line = self.env["sale.order.line"].create(
-                {
-                    "order_id": sale_order.id,
-                    "product_id": self.end_prod_a_red.id,
-                    "product_uom_qty": 10.0,
-                }
-            )
-            move.sale_line_id = sale_line
-
-            self.assertEqual(move.sale_customer_ref, "PO-12345")
--- mrp_campaign_sale/models/__init__.py
+++ mrp_campaign_sale/models/__init__.py
@@ -1,3 +1,4 @@
 from . import mrp_campaign_demand
 from . import mrp_campaign
 from . import sale_order
+from . import stock_move
