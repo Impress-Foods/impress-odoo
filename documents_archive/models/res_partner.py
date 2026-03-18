21f3cc5c39ced1e5b098245134f113255540be41
Cédric Paradis <cparadis@impressfoods.com>
2026-03-18 13:10:11 +0000

    [IMP] documents_archive: pre-commit execution

---
+++ documents_archive/models/res_partner.py
@@ -0,0 +1,25 @@
+import logging
+
+from odoo import models
+
+_logger = logging.getLogger(__name__)
+
+
+class Partner(models.Model):
+    _inherit = "res.partner"
+
+    def action_see_documents(self):
+        self.ensure_one()
+        action = super().action_see_documents()
+        return action
+
+    def _compute_document_count(self):
+        read_group_var = self.env["documents.document"]._read_group(
+            [("partner_id", "in", self.ids), ("archived", "=", False)],
+            groupby=["partner_id"],
+            aggregates=["__count"],
+        )
+
+        document_count_dict = {partner.id: count for partner, count in read_group_var}
+        for record in self:
+            record.document_count = document_count_dict.get(record.id, 0)
