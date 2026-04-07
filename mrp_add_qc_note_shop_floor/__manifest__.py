64cffaacbf1c359d3668c0c6bac87f4b32ab9a6e
Cédric Paradis <cparadis@impressfoods.com>
2026-04-07 18:44:50 +0000

    [IMP] mrp_add_qc_note_shop_floor/: pre-commit execution

---
+++ mrp_add_qc_note_shop_floor/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Add QC Note to shop floor",
+    "version": "17.0.0.1.0",
+    "summary": """ Mrp_add_qc_note_shop_floor Summary """,
+    "author": "Cédric Paradis",
+    "website": "https://github.com/Impress-Foods/impress-odoo",
+    "category": "Hidden",
+    "depends": ["base", "web", "mrp_workorder"],
+    "data": [],
+    "assets": {
+        "web.assets_backend": [
+            "mrp_add_qc_note_shop_floor/static/src/**/*.js",
+            "mrp_add_qc_note_shop_floor/static/src/**/*.xml",
+        ],
+    },
+    "installable": True,
+    "license": "GPL-2",
+}
