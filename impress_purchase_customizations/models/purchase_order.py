799786fe5bd9ae9f5297cd47d69f81aebe74c54f
Cédric Paradis <cparadis@impressfoods.com>
2026-04-08 09:32:48 -0400

    19.0 mig purchase customizations (#238)

---
+++ impress_purchase_customizations/models/purchase_order.py
@@ -0,0 +1,18 @@
+from odoo import models
+from odoo.exceptions import ValidationError
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    def button_unlock(self):
+        self.ensure_none()
+        if (
+            self.env.user.has_group(
+                "impress_purchase_customizations.can_bypass_po_locks"
+            )
+            or self.user_id == self.env.user
+        ):
+            return super().button_unlock()
+        else:
+            raise ValidationError(self.env._("You cannot unlock another user's PO!"))
