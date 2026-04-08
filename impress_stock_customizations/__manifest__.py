190354c4fe55fd2fd4806824895f62e278b276d8
Cédric Paradis <cparadis@impressfoods.com>
2026-04-08 16:16:40 +0000

    [IMP] impress_stock_customizations/: pre-commit execution

---
+++ impress_stock_customizations/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "Impress Stock Customizations",
+    "version": "17.0.0.2.1",
+    "depends": ["base", "stock", "product_expiry"],
+    "author": "Cédric Paradis",
+    "website": "https://github.com/Impress-Foods/impress-odoo",
+    "category": "Inventory",
+    "summary": """
+    Customizations for the stock module developped in-house by Impress Foods SEC
+    """,
+    "license": "GPL-2",
+    # data files always loaded at installation
+    "data": [
+        "reports/impress_stock_customizations_stock_delivery_document_views.xml",
+        "reports/impress_stock_customizations_labels.xml",
+        "reports/online_sale_labels.xml",
+        "views/stock_lot_views.xml",
+        "reports/stock_picking_document_views.xml",
+        "reports/online_sale_labels.xml",
+    ],
+}
---
+++ impress_stock_customizations/models/stock_lot.py
@@ -0,0 +1,26 @@
+import logging
+
+from odoo import api, fields, models
+
+_logger = logging.getLogger(__name__)
+
+
+class StockLot(models.Model):
+    _inherit = "stock.lot"
+
+    life_remaining = fields.Integer(compute="_compute_life_remaining")
+
+    expiration_date = fields.Datetime(tracking=True)
+    use_date = fields.Datetime(tracking=True)
+    removal_date = fields.Datetime(tracking=True)
+    alert_date = fields.Datetime(tracking=True)
+
+    @api.depends("expiration_date")
+    def _compute_life_remaining(self):
+        today = fields.Date.today()
+        for record in self:
+            if not record.use_expiration_date or not record.expiration_date:
+                record.life_remaining = 0
+            else:
+                diff = record.expiration_date.date() - today
+                record.life_remaining = max(diff.days, 0)
