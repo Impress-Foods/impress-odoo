8b78493ceed5664fc64f0907a26bdbf5be89d56d
Cédric Paradis <cparadis@impressfoods.com>
2026-04-09 12:51:01 +0000

    [MIG] impress_emails: Migration to 19.0

--- impress_emails/__manifest__.py
+++ impress_emails/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Emails Templates - Impress Foods",
-    "version": "17.0.1.0.4",
+    "version": "19.0.1.0.0",
     "summary": """ Email templates for Impress Foods """,
     "author": "Cédric Paradis",
     "website": "https://github.com/Impress-Foods/impress-odoo",
