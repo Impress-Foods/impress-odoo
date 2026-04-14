f9030e0bfb97b4e7b8ef0e1cfa6ee363d15645ae
Cédric Paradis <cparadis@impressfoods.com>
2026-04-14 10:38:51 -0400

    19.0 mig mrp fast allocation (#248)

---
+++ mrp_fast_allocation/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "MRP Fast Allocation",
+    "version": "19.0.1.0.0",
+    "summary": """ Adds an action to assign all moves in the allocation
+    report for a production order """,
+    "author": "Cédric Paradis",
+    "website": "https://github.com/Impress-Foods/impress-odoo",
+    "category": "Hidden",
+    "depends": ["base", "mrp"],
+    "data": ["views/mrp_production_server_action.xml"],
+    "installable": True,
+    "license": "GPL-2",
+}
