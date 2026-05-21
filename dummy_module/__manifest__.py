8324cff1052892fc8986893b73efd502aac91be5
Cédric Paradis <cparadis@impressfoods.com>
2026-05-21 18:54:02 +0000

    [19.0][DUM] Added dummy module

---
+++ dummy_module/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "dummy_module",
+    "summary": "Short (1 phrase/line) summary of the module's purpose",
+    "author": "Cédric Paradis",
+    "license": "GPL-2",
+    "website": "https://github.com/Impress-Foods/impress-odoo",
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    "category": "Uncategorized",
+    "version": "19.0.0.0.1",
+    # any module necessary for this one to work correctly
+    "depends": ["base"],
+}
---
+++ dummy_module/models/models.py
@@ -0,0 +1,16 @@
+# from odoo import models, fields, api
+
+
+# class dummy_module(models.Model):
+#     _name = 'dummy_module.dummy_module'
+#     _description = 'dummy_module.dummy_module'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
