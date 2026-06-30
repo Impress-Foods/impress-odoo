41b57a4dc1d8358f4a3ef6907d9335fb919bc252
Cédric Paradis <cedric@cedricparadis.ca>
2026-06-30 21:12:39 +0000

    [19.0][FIX] Fixed broken put in pack workflow in barcode
    Signed-off-by: Cédric Paradis <cedric@cedricparadis.ca>

--- package_consume_stock/models/stock_move_line.py
+++ package_consume_stock/models/stock_move_line.py
@@ -4,6 +4,23 @@ from odoo import models
 class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
+    def _should_display_put_in_pack_wizard(
+        self, package_id, package_type_id, package_name, from_package_wizard
+    ):
+        if self.env.context.get("barcode_view"):
+            if from_package_wizard:
+                return False
+            define_package_type = self._should_set_package()
+            return (
+                define_package_type
+                and not package_id
+                and not package_type_id
+                and not package_name
+            )
+        return super()._should_display_put_in_pack_wizard(
+            package_id, package_type_id, package_name, from_package_wizard
+        )
+
     def _post_put_in_pack_hook(self, package):
         res = super()._post_put_in_pack_hook(package)
         if (
