b622d8e55ebfbea82f7ea927ca068a8f5cc34b40
Cédric Paradis <cedric@cedricparadis.ca>
2026-07-13 09:39:41 -0400

    [19.0][IMP] picking reports (#267)
    Signed-off-by: Cédric Paradis <cedric@cedricparadis.ca>

--- impress_stock_customizations/__manifest__.py
+++ impress_stock_customizations/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Impress Stock Customizations",
-    "version": "19.0.1.0.1",
+    "version": "19.0.1.1.1",
     "depends": ["base", "stock", "product_expiry"],
     "author": "Cédric Paradis",
     "website": "https://github.com/Impress-Foods/impress-odoo",
@@ -18,6 +18,8 @@
         "reports/report_templates.xml",
         "views/stock_lot_views.xml",
         "views/res_partner_views.xml",
+        "reports/stock_picking_with_checks_report.xml",
+        "reports/report_deliveryslip.xml",
         "views/product_template_views.xml",
     ],
     "assets": {
