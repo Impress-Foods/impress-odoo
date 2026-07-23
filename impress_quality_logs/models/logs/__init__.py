592c8482d7d0ed526f2644af3939a3368fab10fa
Cédric Paradis <cedric@cedricparadis.ca>
2026-07-23 13:47:59 +0000

    [19.0][UPD] Removed coding log line model

--- impress_quality_logs/models/logs/__init__.py
+++ impress_quality_logs/models/logs/__init__.py
@@ -4,7 +4,6 @@ from . import weight_log_line
 from . import metal_log_line
 from . import xray_log_line
 
-# from . import coding_log_line
 from . import hpp_log
 from . import weight_log
 from . import metal_log
