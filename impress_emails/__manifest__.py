8448f35ed6fbc345f99d3efac94c11cbbd1e7b01
Cédric Paradis <cedric@cedricparadis.ca>
2026-07-28 09:01:38 -0400

    [19.0][UPD] Added Monday pickup to obibox email for impress and A+ (#283)

--- impress_emails/__manifest__.py
+++ impress_emails/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Emails Templates - Impress Foods",
-    "version": "19.0.1.0.0",
+    "version": "19.0.1.1.0",
     "summary": """ Email templates for Impress Foods """,
     "author": "Cédric Paradis",
     "website": "https://github.com/Impress-Foods/impress-odoo",
