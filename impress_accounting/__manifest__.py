db34baaf510e2346b4cf4378685acab01b5ee882
Cédric Paradis <cedric@cedricparadis.ca>
2026-08-12 12:33:19 +0000

    [19.0][UPD] Updated ca top check layout to match physical checks
    Signed-off-by: Cédric Paradis <cedric@cedricparadis.ca>

--- impress_accounting/__manifest__.py
+++ impress_accounting/__manifest__.py
@@ -1,15 +1,18 @@
 {
     "name": "Impress Accounting",
-    "version": "19.0.1.0.2",
+    "version": "19.0.1.0.3",
     "summary": """ Accounting specific changes for Impress Foods """,
     "author": "Cédric Paradis",
     "website": "https://github.com/Impress-Foods/impress-odoo",
     "category": "Hidden",
-    "depends": ["base", "account", "account_reports"],
+    "depends": ["base", "account", "account_reports", "l10n_ca_check_printing"],
     "data": ["views/res_partner_views.xml"],
     "assets": {
         "web.assets_backend": [
             "impress_accounting/static/src/account_report.xml",
+        ],
+        "web.report_assets_common": [
+            "impress_accounting/static/src/report_check_top.scss",
         ],
     },
     "application": False,
