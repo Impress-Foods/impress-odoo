e60b776e27bcf79e696142f47d362dcd7d19add6
Cédric Paradis <cedric@cedricparadis.ca>
2026-08-24 18:30:57 +0000

    [19.0][IMP] Commercial Invoice report for sale orders
    Signed-off-by: Cédric Paradis <cedric@cedricparadis.ca>

--- impress/__manifest__.py
+++ impress/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Impress Customizations",
-    "version": "19.0.1.0.0",
+    "version": "19.0.1.0.1",
     "depends": ["base", "web"],
     "author": "Cédric Paradis",
     "website": "https://github.com/Impress-Foods/impress-odoo",
--- impress_sales_customizations/models/sale_order.py
+++ impress_sales_customizations/models/sale_order.py
@@ -5,3 +5,5 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     delivery_zip = fields.Char(related="partner_shipping_id.zip", store=True)
+
+    port_of_entry = fields.Char()
