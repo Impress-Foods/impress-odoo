6defdad34daf1b78a75efad1251e908b5b0b92b2
Cédric Paradis <cedric@cedricparadis.ca>
2026-08-26 17:55:23 +0000

    [19.0][IMP] Shop floor wizard for printing_connector_mrp
    Signed-off-by: Cédric Paradis <cedric@cedricparadis.ca>

--- printing_connector_mrp/models/quality_check.py
+++ printing_connector_mrp/models/quality_check.py
@@ -1,9 +1,16 @@
-from odoo import models
+import logging
+
+from odoo import fields, models
 from odoo.exceptions import ValidationError
+from odoo.fields import Domain
+
+_logger = logging.getLogger(__name__)
 
 
 class QualityCheck(models.Model):
     _inherit = "quality.check"
+
+    test_report_type = fields.Selection(related="point_id.test_report_type")
 
     def _get_product_label_action(self, report_type):
         if report_type == "api":
@@ -19,7 +26,7 @@ class QualityCheck(models.Model):
 
     def _send_api_label(self):
         self.ensure_one()
-
+        _logger.debug(self.env.context)
         report = self.point_id.report_id
 
         if report.report_type != "api":
@@ -42,7 +49,7 @@ class QualityCheck(models.Model):
             raise ValidationError(self.env._("Must specify lot for label printing"))
 
         res = report.report_action(
-            lot_id, data={"qty": qty, "printer": self._get_printer()}
+            lot_id, data={"qty": qty, "printer": self._get_printer_name()}
         )
         res["id"] = report.id
         return res
@@ -61,14 +68,36 @@ class QualityCheck(models.Model):
                 self.env.context.get("printing_printer_override")
             )
             if printer:
-                return printer.technical_name
+                return printer
 
         if self.point_id.printer_id:
-            return self.point_id.printer_id.technical_name
+            return self.point_id.printer_id
 
         if self.workcenter_id.default_printer_id:
-            return self.workcenter_id.default_printer_id.technical_name
+            return self.workcenter_id.default_printer_id
 
         raise ValidationError(
             self.env._("No printer could be found for this print job!")
         )
+
+    def _get_printer_name(self):
+        self.ensure_one()
+        return self._get_printer().technical_name
+
+    def get_print_data(self):
+        self.ensure_one()
+        report = self.point_id.report_id.print_report_id
+
+        domain = Domain("active", "=", True)
+
+        if report.print_server_id:
+            domain += Domain("server_id", "=", report.print_server_id.id)
+
+        printers = self.env["print.printer"].search_read(domain, ["name"])
+
+        return {
+            "printers": printers,
+            "printer_id": self._get_printer().id,
+            "qty": self._get_print_qty(),
+            "data": self.point_id.report_id.print_report_id._render_json_payload(self),
+        }
