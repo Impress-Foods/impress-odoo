988a7a7eb74b96aecd8868d946281d9ec95593cb
Cédric Paradis <cedric@cedricparadis.ca>
2026-09-04 12:57:03 -0400

    [19.0][IMP] Added kit count in online_sales_summary report (#298)
    Signed-off-by: Cédric Paradis <cedric@cedricparadis.ca>

--- impress_stock_customizations/reports/report_online_sales_summary_report.py
+++ impress_stock_customizations/reports/report_online_sales_summary_report.py
@@ -15,12 +15,17 @@ class OnlineSalesSummaryReport(models.AbstractModel):
         docs = self.env["stock.picking"].browse(docids)
         pickings = []
         for picking in docs:
+            carrier = getattr(picking, "carrier_id", False)
+            order = getattr(picking, "sale_id", False)
+
             values = {
                 "name": picking.display_name,
-                "carrier": picking.carrier_id.name,
                 "client": picking.partner_id.name,
-                "order": picking.sale_id.name,
+                "carrier": carrier.name if carrier else "",
+                "order": order.name if order else "",
             }
+
+            _logger.debug(values)
             products_by_category = defaultdict(int)
             for line in picking.move_ids:
                 products_by_category[line.product_category_id.name] += (
@@ -28,6 +33,19 @@ class OnlineSalesSummaryReport(models.AbstractModel):
                 )
             values["products_by_category"] = dict(sorted(products_by_category.items()))
             values["total"] = int(sum(products_by_category.values()))
+
+            kits = defaultdict(float)
+            for move in picking.move_ids:
+                bom_line = move.bom_line_id
+                if (
+                    bom_line
+                    and bom_line.bom_id
+                    and len(bom_line.bom_id.bom_line_ids) > 1
+                ):
+                    kit_name = bom_line.bom_id.product_tmpl_id.name
+                    kits[kit_name] = move.product_uom_qty / bom_line.product_qty
+            values["kits"] = {k: int(v) for k, v in sorted(kits.items())}
+
             pickings.append(values)
         _logger.debug(pickings)
         return {
