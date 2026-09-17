d3ecf5812f7d7f963f6803fc6cda7674c8703fde
Cédric Paradis <cedric@cedricparadis.ca>
2026-09-17 13:46:34 +0000

    [19.0]FIX] Changed campaign menu sequence

--- mrp_campaign/__manifest__.py
+++ mrp_campaign/__manifest__.py
@@ -1,7 +1,7 @@
 # pragma: no coverage
 {
     "name": "Manufacturing Campaigns",
-    "version": "19.0.1.0.1",
+    "version": "19.0.1.0.2",
     "summary": """ Base module for manufacturing campaigns """,
     "author": "Cédric Paradis",
     "website": "https://github.com/Impress-Foods/impress-odoo",
