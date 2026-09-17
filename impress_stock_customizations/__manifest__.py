bb419129fbfbf34331b5e70d06e502b11fdf90ed
Cédric Paradis <cedric@cedricparadis.ca>
2026-09-17 16:18:42 -0400

    [19.0][IMP] Stock reports improvements (picking with checks + deliveryslip) (#312)

--- impress_stock_customizations/__manifest__.py
+++ impress_stock_customizations/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Impress Stock Customizations",
-    "version": "19.0.1.3.2",
+    "version": "19.0.1.3.0",
     "depends": [
         "base",
         "impress",
@@ -13,7 +13,6 @@
     "website": "https://github.com/Impress-Foods/impress-odoo",
     "category": "Inventory",
     "data": [
-        "reports/impress_stock_customizations_stock_delivery_document_views.xml",
         "reports/impress_stock_customizations_labels.xml",
         "reports/online_sale_labels.xml",
         "reports/stock_picking_document_views.xml",
